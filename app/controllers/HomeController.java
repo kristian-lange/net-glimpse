67c4eb9cf9115951686660a2c57074471685fdc4
Kristian Lange <k.lange@searchmetrics.com>
2017-06-15 18:45:31 +0200

    build ws

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -29,135 +29,158 @@ import java.util.concurrent.CompletableFuture;
 @Singleton
 public class HomeController extends Controller {
 
-	private static final ALogger LOGGER = Logger.of(HomeController.class);
+    private static final ALogger LOGGER = Logger.of(HomeController.class);
 
-	private final HttpExecutionContext httpExecutionContext;
-	private final PcapInitializer pcapInitializer;
+    private final HttpExecutionContext httpExecutionContext;
+    private final PcapInitializer pcapInitializer;
 
-	@Inject
-	@Named("websocket-dispatcher-actor")
-	private ActorRef webSocketDispatcherActorRef;
+    @Inject
+    @Named("websocket-dispatcher-actor")
+    private ActorRef webSocketDispatcherActorRef;
 
-	@Inject
-	public HomeController(HttpExecutionContext httpExecutionContext,
-			PcapInitializer pcapInitializer) {
-		this.httpExecutionContext = httpExecutionContext;
-		this.pcapInitializer = pcapInitializer;
-	}
+    @Inject
+    public HomeController(HttpExecutionContext httpExecutionContext,
+            PcapInitializer pcapInitializer) {
+        this.httpExecutionContext = httpExecutionContext;
+        this.pcapInitializer = pcapInitializer;
+    }
 
-	public Result index() {
-		return ok(views.html.index.render());
-	}
+    public Result index() {
+        return ok(views.html.index.render());
+    }
 
-	public Result p5visu() {
-		return ok(views.html.p5visu.render());
-	}
+    public Result p5visu() {
+        return ok(views.html.p5visu.render());
+    }
 
-	// TODO support multiple clients
-	// TODO use Akka Stream WebSockets
-	public LegacyWebSocket<JsonNode> ether() {
-		return WebSocket.whenReady((in, out) -> {
-			// In extra thread write pcap to WebSocket.out
-			Runnable r = () -> {
-				pcapToWebSocket(pcapInitializer.getPcapHandle(), out);
-			};
-			CompletableFuture pcapToWebSocketFuture = CompletableFuture
-					.runAsync(r, httpExecutionContext.current());
+    // TODO support multiple clients
+    // TODO use Akka Stream WebSockets
+    public LegacyWebSocket<JsonNode> ether() {
+//        return WebSocket.whenReady((in, out) -> {
+//            // In extra thread write pcap to WebSocket.out
+//            Runnable r = () -> {
+//                pcapToWebSocket(pcapInitializer.getPcapHandle(), out);
+//            };
+//            CompletableFuture pcapToWebSocketFuture = CompletableFuture
+//                    .runAsync(r, httpExecutionContext.current());
+//
+//            // For each event received on the socket
+//            in.onMessage(System.out::println);
+//
+//            // When the socket is closed
+//            in.onClose(() -> {
+//                pcapToWebSocketFuture.cancel(true);
+//                LOGGER.info("Closed WebSocket");
+//            });
+//
+//
+//            Props.create(WebSocketActor.class, out, webSocketDispatcherActorRef);
+//
+//            LOGGER.info(
+//                    "Opened WebSocket and writing network interface {} into it",
+//                    pcapInitializer.getNetworkInterfaceName());
+//        });
+        return buildWebSocket(webSocketDispatcherActorRef);
+    }
 
-			// For each event received on the socket
-			in.onMessage(System.out::println);
+    public static <T> LegacyWebSocket<JsonNode> buildWebSocket(ActorRef dispatcher) {
+        return new LegacyWebSocket<JsonNode>() {
+            public void onReady(WebSocket.In<JsonNode> in, WebSocket.Out<JsonNode> out) {
+            }
 
-			// When the socket is closed
-			in.onClose(() -> {
-				pcapToWebSocketFuture.cancel(true);
-				LOGGER.info("Closed WebSocket");
-			});
+            public boolean isActor() {
+                return true;
+            }
 
-			Props.create(WebSocketActor.class, out,	webSocketDispatcherActorRef);
+            public Props actorProps(ActorRef out) {
+                try {
+                    return Props.create(WebSocketActor.class, out, dispatcher);
+                } catch(RuntimeException e) {
+                    throw e;
+                } catch(Error e) {
+                    throw e;
+                } catch(Throwable t) {
+                    throw new RuntimeException(t);
+                }
+            }
+        };
+    }
 
-			LOGGER.info(
-					"Opened WebSocket and writing network interface {} into it",
-					pcapInitializer.getNetworkInterfaceName());
-		});
-	}
+    private void pcapToWebSocket(PcapHandle pcapHandle,
+            WebSocket.Out<JsonNode> out) {
+        while(true) {
+            try {
+                Packet packet = pcapHandle.getNextPacket();
+                if(packet != null) {
+                    writePacketToWebSocketOut(packet, out, pcapHandle);
 
-	private void pcapToWebSocket(PcapHandle pcapHandle,
-			WebSocket.Out<JsonNode> out) {
-		while (true) {
-			try {
-				Packet packet = pcapHandle.getNextPacket();
-				if (packet != null) {
-					writePacketToWebSocketOut(packet, out, pcapHandle);
+                }
+            } catch(NotOpenException e) {
+                break;
+            }
+        }
+    }
 
+    private void writePacketToWebSocketOut(Packet packet,
+            WebSocket.Out<JsonNode> out, PcapHandle pcapHandle) {
+        ObjectNode outNode = Json.newObject();
+        outNode.put("timestamp", pcapHandle.getTimestamp().toString());
+        fillNodeWithEthernetPacket(outNode, packet.get(EthernetPacket.class));
+        fillNodeWithIpPacket(outNode, packet.get(IpPacket.class));
+        out.write(outNode);
+        webSocketDispatcherActorRef.tell(outNode, ActorRef.noSender());
+    }
 
-				}
-			} catch (NotOpenException e) {
-				break;
-			}
-		}
-	}
+    private void fillNodeWithEthernetPacket(ObjectNode outNode,
+            EthernetPacket ethernetPacket) {
+        if(ethernetPacket != null) {
+            EthernetPacket.EthernetHeader header =
+                    ethernetPacket.getHeader();
+            outNode.put("macSrcAddr", header.getSrcAddr().toString());
+            outNode.put("macDstAddr", header.getDstAddr().toString());
+            outNode.put("macType", header.getType().valueAsString());
+        }
+    }
 
-	private void writePacketToWebSocketOut(Packet packet,
-			WebSocket.Out<JsonNode> out, PcapHandle pcapHandle) {
-		ObjectNode outNode = Json.newObject();
-		outNode.put("timestamp", pcapHandle.getTimestamp().toString());
-		fillNodeWithEthernetPacket(outNode, packet.get(EthernetPacket.class));
-		fillNodeWithIpPacket(outNode, packet.get(IpPacket.class));
-		out.write(outNode);
-		webSocketDispatcherActorRef.tell(outNode, ActorRef.noSender());
-	}
+    private void fillNodeWithIpPacket(ObjectNode outNode, IpPacket ipPacket) {
+        if(ipPacket != null) {
+            IpPacket.IpHeader header = ipPacket.getHeader();
+            outNode.put("ipSrcAddr", header.getSrcAddr().toString());
+            outNode.put("ipDstAddr", header.getDstAddr().toString());
+            outNode.put("ipProtocol",
+                    header.getProtocol().valueAsString());
+            outNode.put("ipVersion",
+                    header.getVersion().valueAsString());
+        }
+    }
 
-	private void fillNodeWithEthernetPacket(ObjectNode outNode,
-			EthernetPacket ethernetPacket) {
-		if (ethernetPacket != null) {
-			EthernetPacket.EthernetHeader header =
-					ethernetPacket.getHeader();
-			outNode.put("macSrcAddr", header.getSrcAddr().toString());
-			outNode.put("macDstAddr", header.getDstAddr().toString());
-			outNode.put("macType", header.getType().valueAsString());
-		}
-	}
+    public LegacyWebSocket<JsonNode> ether2() {
+        return createWebSocket();
+    }
 
-	private void fillNodeWithIpPacket(ObjectNode outNode, IpPacket ipPacket) {
-		if (ipPacket != null) {
-			IpPacket.IpHeader header = ipPacket.getHeader();
-			outNode.put("ipSrcAddr", header.getSrcAddr().toString());
-			outNode.put("ipDstAddr", header.getDstAddr().toString());
-			outNode.put("ipProtocol",
-					header.getProtocol().valueAsString());
-			outNode.put("ipVersion",
-					header.getVersion().valueAsString());
-		}
-	}
+    private LegacyWebSocket<JsonNode> createWebSocket() {
+        return new LegacyWebSocket<JsonNode>() {
 
-	public LegacyWebSocket<JsonNode> ether2() {
-		return createWebSocket();
-	}
+            @Override
+            public void onReady(final WebSocket.In<JsonNode> in, final WebSocket.Out<JsonNode> out) {
+            }
 
-	private LegacyWebSocket<JsonNode> createWebSocket() {
-		return new LegacyWebSocket<JsonNode>() {
+            public boolean isActor() {
+                return true;
+            }
 
-			@Override
-			public void onReady(final WebSocket.In<JsonNode> in, final WebSocket.Out<JsonNode> out) {
-			}
-
-			public boolean isActor() {
-				return true;
-			}
-
-			public Props actorProps(ActorRef out) {
-				try {
-					return Props.create(WebSocketActor.class, out);
-				} catch (RuntimeException e) {
-					throw e;
-				} catch (Error e) {
-					throw e;
-				} catch (Throwable t) {
-					throw new RuntimeException(t);
-				}
-			}
-		};
-	}
-
+            public Props actorProps(ActorRef out) {
+                try {
+                    return Props.create(WebSocketActor.class, out);
+                } catch(RuntimeException e) {
+                    throw e;
+                } catch(Error e) {
+                    throw e;
+                } catch(Throwable t) {
+                    throw new RuntimeException(t);
+                }
+            }
+        };
+    }
 
 }
